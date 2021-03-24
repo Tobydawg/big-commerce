2a2138be0c6be53b268d63d46fb80ce5b17b27c9
Toby <agentalliancela33@gmail.com>
2021-03-23 19:09:59 -0700

    init

---
+++ routes/api/category-routes.js
@@ -0,0 +1,163 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+// The /api/categories endpoint
+router.get('/', (req, res) => {
+  // find all categories
+  Category.findAll({
+  // be sure to include its associated Products
+    include: [Product]
+  }).then((dbPost) =>
+  res.json(dbPost))
+.catch((err )=> {
+  console.log(err);
+  res.status(400).json(err);
+});
+});
+router.get('/:id', (req, res) => {
+  // find one category by its id value
+  Category.findOne({
+  // be sure to include its associated Products
+  where: {
+    id: req.params.id
+  },
+  include: [Product]
+  }).then((dbPost) =>
+  res.json(dbPost))
+.catch((err )=> {
+  console.log(err);
+  res.status(400).json(err);
+});
+});
+router.post('/', (req, res) => {
+  // create a new category
+  // expects category_name
+  Category.create({
+    category_name: req.body.category_name
+  }).then((dbPost) =>
+  res.json(dbPost))
+.catch((err )=> {
+  console.log(err);
+  res.status(400).json(err);
+});
+});
+router.put('/:id', (req, res) => {
+  // update a category by its id value
+  // pass in req.body instead to only update what's passed through
+  Category.update(req.body, {
+    // individualHooks: true,
+    where: {
+      id: req.params.id
+    }
+  }).then((dbPost) =>
+  res.json(dbPost))
+.catch((err )=> {
+  console.log(err);
+  res.status(400).json(err);
+});
+});
+router.delete('/:id', (req, res) => {
+  // delete a category by its id value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then((dbPost) =>
+  res.json(dbPost))
+.catch((err )=> {
+  console.log(err);
+  res.status(400).json(err);
+});
+});
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const router = require("express").Router();
+// const { Category, Product } = require("../../models");
+
+// // The `/api/categories` endpoint
+
+// router.get("/", (req, res) => {
+//   // find all categories
+//   // be sure to include its associated Products
+//   Category.findAll({
+//   include: [Product]
+//   })
+//     .then((dbCategoryData) => res.json(dbCategoryData))
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
+// //????????????????????????????????????? confused about attribues and where in the same little chunk
+
+// router.get("/:id", (req, res) => {
+//   // find one category by its `id` value
+//   // be sure to include its associated Products
+//   Category.findOne({
+//     where: {
+//       id: req.params.id
+//     },
+//     //include statement
+//     include: [Product]
+//   })
+//     .then((dbCategoryData) => res.json(dbCategoryData))
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
+// router.post("/", (req, res) => {
+//   // create a new category
+//   Category.create({
+//   category_name: req.body.category_name
+
+
+//     .then((dbCategoryData) => res.json(dbCategoryData))
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     })
+//   });
+// });
+
+// router.put("/:id", (req, res) => {
+//   // update a category by its `id` value
+//   Category.update(req.body, {
+//     where: {
+//       id: req.params.id
+//     }
+//   })
+//     .then((dbCategoryData) => res.json(dbCategoryData))
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
+// router.delete("/:id", (req, res) => {
+//   // delete a category by its `id` value
+//   Category.destroy({
+//     where: {
+//       id: req.params.id
+//     }
+//   })
+//     .then((dbCategoryData) => res.json(dbCategoryData))
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+// module.exports = router;
---
+++ routes/api/tag-routes.js
@@ -0,0 +1,200 @@
+// const router = require("express").Router();
+
+// const { Tag, Product, ProductTag } = require("../../models");
+
+// // The `/api/tags` endpoint
+
+// router.get("/", (req, res) => {
+//   //find all tags
+//   tag.findAll({
+//     include: [
+//       Product,
+//       {
+//         model: Tag,
+//         through: ProductTag,
+//         as: 'product_tag_id'
+//       }
+//     ]
+//   })
+//   .then((dbTagData) => res.json(dbTagData))
+//   .catch((err) =>{
+//     console.log(err);
+//     res.status(400).json(err);
+
+
+//   });
+// });
+// // be sure to include its associated Product data
+
+// router.get("/:id", (req, res) => {
+//   // find a single tag by its `id`
+//   // be sure to include its associated Product data
+//   Tag.findOne(
+//     {
+//       where: {
+//         id: req.params.id,
+//       },
+//     },
+//     {
+//       include: [
+//         Product,
+//         {
+//           model: Tag,
+//           through: ProductTag,
+//           as: 'product_tag_id',
+//         },
+//       ],
+//     }
+//   )
+//     .then((dbTagData) => res.json(dbTagData))
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(400).json(err);
+//     });
+// });
+
+// router.post("/", (req, res) => {
+//   // create a new tag
+//   Tag.create(req.body, {
+
+//     //?????????????????????????????not sure
+//     where:{
+//       id: req.params.id,
+//     },
+//     })
+//     .then((dbTagData) => res.json (dbTagData))
+
+
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+// The `/api/tags` endpoint
+router.get('/', (req, res) => {
+  // find all tags
+  Tag.findAll({
+    include: {
+      // be sure to include its associated Product data
+      model: Product,
+    }
+  }).then((dbPost) =>
+  res.json(dbPost))
+.catch((err )=> {
+  console.log(err);
+  res.status(400).json(err);
+});
+});
+router.get('/:id', (req, res) => {
+  // find a single tag by its `id`
+  Tag.findOne({
+    where: {
+      id:req.params.id
+    },
+    // be sure to include its associated Product data
+    include: {
+      model: Product
+    }
+  }).then((dbPost) =>
+  res.json(dbPost))
+.catch((err )=> {
+  console.log(err);
+  res.status(400).json(err);
+});
+});
+
+
+
+router.post('/', (req, res) => {
+  // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  .then((dbPost) =>
+    res.json(dbPost))
+  .catch((err )=> {
+    console.log(err);
+    res.status(400).json(err);
+  });
+});
+
+
+
+
+
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+  Tag.update({
+    tag_name: req.body.tag_name
+  },{
+    where: {
+      id: req.params.id
+    }
+  }).then((dbPost) =>
+  res.json(dbPost))
+.catch((err )=> {
+  console.log(err);
+  res.status(400).json(err);
+});
+});
+
+
+
+
+
+
+router.delete('/:id', (req, res) => {
+  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then((dbPost) =>
+  res.json(dbPost))
+.catch((err )=> {
+  console.log(err);
+  res.status(400).json(err);
+});
+});
+module.exports = router;
+
+
+
+
+
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+    
+
+
+//   });
+// });
+
+// router.put("/:id", (req, res) => {
+//   // update a tag's name by its `id` value
+//   Tag.update(req.res, {
+//     where: {
+//       id: req.params.id,
+//     }
+//   })
+//   .then((dbTagData) => res.json(dbTagData))
+//   .catch((err) => {
+//     console.log(err);
+//     res.status(500).json(err);
+//   });
+// });
+
+// router.delete("/:id", (req, res) => {
+//   // delete on tag by its `id` value
+//   Tag.destroy(req.res,  {
+//     where: {
+//       id: req.params.id,
+//     },
+//   })
+//   .then((dbTagData) => res.json (dbTagData))
+//   .catch((err) =>{
+//     console.log(err);
+//     res.status(500).json(err);
+//   })
+// });
+
+// module.exports = router;
